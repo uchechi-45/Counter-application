d6c55d00f70c3d7140943757452c3fefacd45779
uchechi <isuuchechi@gmail.com>
2023-11-24 11:17:01 +0100

    counter application

---
+++ index.js
@@ -0,0 +1,131 @@
+ const count=document.getElementById('count');
+let counter=0;
+
+function increaseBtn(){
+    counter = counter + 1;
+    count.innerHTML=counter
+};
+
+
+function decreasebtn(){
+    counter = counter - 1;
+    count.innerHTML=counter;
+
+};
+
+
+function Reset(){
+    counter =0;
+    count.innerHTML = counter;
+};
+
+function doublex2(){
+    counter=counter *2;
+    count.innerHTML=counter;
+};
+
+
+function square(){
+    counter=counter **2;
+    count.innerHTML=counter;
+};
+    
+function x5(){
+    counter=counter *5;
+    count.innerHTML=counter;
+};
+
+function cube(){
+    counter=counter *3;
+    count.innerHTML=counter;
+};
+ 
+
+function x10() {
+    counter = counter * 10;
+    count.innerHTML=counter;
+};
+   
+
+
+function x15() {
+    counter = counter *15;
+    count.innerHTML=counter;
+};
+   
+
+
+function squareroot(){
+    counter = Math.sqrt(counter);
+    count.innerHTML=counter;
+}; 
+   
+
+
+function x25(){
+    counter =counter *25;
+    count.innerHTML=counter;
+}; 
+   
+
+function x3 (){
+    counter =counter *3;
+    count.innerHTML=counter;
+}; 
+   
+function x7 (){
+    counter = counter *7;
+    count.innerHTML=counter;
+
+
+
+}; function x9(){
+    counter = counter * 9;
+    count.innerHTML=counter;
+}; 
+   
+   
+
+function x12(){
+    counter = counter * 12;
+    count.innerHTML=counter;
+}; 
+   
+
+
+function x50 (){
+    counter =counter *50 ;
+    count.innerHTML=counter;
+}; 
+   
+
+
+
+function plus(){
+    counter =counter +4;
+    count.innerHTML=counter ;
+}; 
+   
+
+
+
+function divide (){
+    counter =counter / 5;
+    count.innerHTML=counter;
+}; 
+   
+
+function sl(){
+    counter =counter / 15;
+    count.innerHTML=counter;
+}; 
+   
+
+
+function divide35 (){
+    counter =counter /35;
+    count.innerHTML=counter;
+}; 
+   
+
+
